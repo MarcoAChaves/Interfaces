c173d572e4707e1e57917da40be69baa5a3da06b
Marco Chaves <macschaves@gmail.com>
2023-12-06 15:02:34 -0300

    Herança multipla com Interfaces

---
+++ src/HerançaMultipla_Interfaces/application/Program.java
@@ -0,0 +1,18 @@
+package HerançaMultipla_Interfaces.application;
+
+import HerançaMultipla_Interfaces.devices.Printer;
+import HerançaMultipla_Interfaces.devices.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Printer p = new Printer("1080");
+        p.processDoc("My Letter");
+        p.print("My Letter");
+
+        Scanner s = new Scanner("2003");
+        s.processDoc("My Email");
+        System.out.println("Scan result: " + s.scan());
+    }
+}
